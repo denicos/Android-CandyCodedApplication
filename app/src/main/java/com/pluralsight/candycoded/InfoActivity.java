05fa4119d2d615f38f2a5429a14c276c574e6fcf
Denicos <nicoswaves.mn@gmail.com>
2019-10-26 15:03:20 +0300

    info activity changed

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -3,6 +3,7 @@ package com.pluralsight.candycoded;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -22,6 +23,7 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
+
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
